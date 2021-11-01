6080589bb377543365138284fac8fdec5aa46483
JRamos29 <malebolge@gmail.com>
2021-11-01 18:30:59 -0300

    Implemented MenuItem component. Implemented icons/index.tsx. Modified LateralMenu component.

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,7 +1,18 @@
+import MenuItem from './MenuItem';
+import { IconHome, IconSettings, IconNotification } from '../icons/index';
+
 export default function LateralMenu() {
   return (
     <aside>
-      <h1>Menu</h1>
+      <ul>
+        <MenuItem url="/" text="Home" icon={IconHome} />
+        <MenuItem url="/settings" text="Settigns" icon={IconSettings} />
+        <MenuItem
+          url="/notifications"
+          text="Notifications"
+          icon={IconNotification}
+        />
+      </ul>
     </aside>
   );
 }

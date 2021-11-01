ceef378870cd19a4229a7b2666dd6cddead2142f
JRamos29 <malebolge@gmail.com>
2021-11-01 19:07:30 -0300

    Implemented Logo component. Modified Title component. Modified LateralMenu component.

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,9 +1,18 @@
 import MenuItem from './MenuItem';
+import Logo from './Logo';
 import { IconHome, IconSettings, IconNotification } from '../icons/index';
 
 export default function LateralMenu() {
   return (
     <aside>
+      <div
+        className={`
+        flex flex-col items-center justify-center
+      bg-gradient-to-r from-indigo-500 to-purple-800
+      h-20 w-20`}
+      >
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" text="Home" icon={IconHome} />
         <MenuItem url="/settings" text="Settigns" icon={IconSettings} />

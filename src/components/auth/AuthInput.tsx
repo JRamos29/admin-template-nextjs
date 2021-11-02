d33cbd0fcbbb8c01861d0bb0bfbe8792f1171175
JRamos29 <malebolge@gmail.com>
2021-11-02 17:35:45 -0300

    Implemented styles for AuthInput component. Implemented buttons for auth page.

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,16 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
   return props.shouldNotRender ? null : (
-    <div className={`flex flex-col`}>
+    <div className={`flex flex-col mt-4`}>
       <label>{props.label}</label>
       <input
         type={props.type ?? 'text'}
         value={props.value}
         onChange={(e) => props.onChange?.(e.target.value)}
         required={props.required}
+        className={`px-4 py-3 rounded-lg bg-gray-200 mt-2
+        border focus:border-blue-500 focus:bg-white
+        focus:outline-none`}
       />
     </div>
   );
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -7,31 +7,62 @@ export default function Auth() {
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
 
+  function submit() {
+    if (mode === 'login') {
+      console.log('login');
+    } else {
+      console.log('signup');
+    }
+  }
+
   return (
-    <div>
-      <h1>Auth</h1>
-      <AuthInput
-        label="Email"
-        type="email"
-        value={email}
-        onChange={setEmail}
-        required
-      />
-      <AuthInput
-        label="Password"
-        type="password"
-        value={password}
-        onChange={setPassword}
-        required
-      />
-      <AuthInput
-        label="Confirm Password"
-        type="password"
-        value={confirmPassword}
-        onChange={setConfirmPassword}
-        required
-        shouldNotRender={mode === 'login'}
-      />
+    <div className={`flex flex-col h-screen items-center justify-center`}>
+      <div className={`w-1/2`}>
+        <h1
+          className={`
+        text-xl font-bold mb-5
+      `}
+        >
+          {mode === 'login'
+            ? 'Login to access your account'
+            : 'Sign-up to create an account'}
+        </h1>
+        <AuthInput
+          label="Email"
+          type="email"
+          value={email}
+          onChange={setEmail}
+          required
+        />
+        <AuthInput
+          label="Password"
+          type="password"
+          value={password}
+          onChange={setPassword}
+          required
+        />
+        <AuthInput
+          label="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={setConfirmPassword}
+          required
+          shouldNotRender={mode === 'login'}
+        />
+        <button
+          className={`w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6`}
+          onClick={submit}
+        >
+          {mode === 'login' ? 'Login' : 'Sign-Up'}
+        </button>
+        <hr className={`my-6 border-gray-300 w-full`} />
+        <button
+          className={`w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3`}
+          onClick={submit}
+        >
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 }
